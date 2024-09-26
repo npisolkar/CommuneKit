e5f0bc749cc919d23a0cf4f3a5d6accf82cb989e
Mingfan Xie <140193345+IIIIVIII@users.noreply.github.com>
2024-09-26 14:30:15 -0400

    Add files via upload

---
+++ CommuneKitApplication.java
@@ -0,0 +1,13 @@
+package com.communkit.communekit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuneKitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommuneKitApplication.class, args);
+	}
+
+}
