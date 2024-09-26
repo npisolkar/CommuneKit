8c6cfeed2bdd738f01d6980e4cbe1a4ef6b640a0
Ashe-w <ashley.w.cwc@gmail.com>
2024-09-26 14:28:24 -0400

    Added item reviews

--- src/Item.java
+++ src/Item.java
@@ -5,6 +5,7 @@ public class Item {
     private String itemCategory;
     private int itemRating;
     private boolean itemStatus;
+    private ArrayList<Review> reviews;
 
     public Item(String itemName, String itemDescription, String itemID, String itemCategory, int itemRating) {
         this.itemName = itemName;
@@ -47,5 +48,23 @@ public class Item {
     }
     public void setItemStatus(boolean itemStatus) {
         this.itemStatus = itemStatus;
+    }
+    public ArrayList<Review> getReviews() {
+        return reviews;
+    }
+    public void addReviews(Review review){
+        reviews.add(review);
+    }
+
+    public void updateRating() {
+        if (reviews.size() == 0) {
+            return;
+        }
+
+        float sum = 0;
+        for (Review review : reviews) {
+            sum += review.getRating();
+        }
+        rating = sum / reviews.size();
     }
 }
