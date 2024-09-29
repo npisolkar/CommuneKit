019d885831c11a66d4a5478eeef489ebce7bf98a
Edward Bi <bi43@purdue.edu>
2024-09-29 16:43:59 -0400

    my work for this week

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+// @ts-ignore
+import { useState } from 'react'
+import './styles.css'
+import Home from "./Home.jsx"
+import Search from "./Search.jsx"
+import Notifications from "./Notifications.jsx"
+import Profile from "./Profile.jsx"
+import { BrowserRouter as Router, Route, Link, Routes,} from "react-router-dom";
+
+export default function App() {
+  return (
+      <>
+      <Router>
+          <div>
+              <div className="menu-bar" id="navbar">
+                  <Link id="logo" to="/">
+                      <img src="/CommuneKit Logo.png" alt="logo"></img>
+                  </Link>
+                  <Link to="/search" id="search-button"><button>Search</button></Link>
+                  <Link to="/notifications" id="notif-button"><button>Notifications</button></Link>
+                  <Link to="/profile" id="profile-button"><button>Profile</button></Link>
+                  <MenuButton />
+              </div>
+
+              <Routes>
+                  <Route path="/search" element={<Search />}/>
+                  <Route path="/notifications" element={<Notifications />} />
+                  <Route path="/profile" element={<Profile isOwn={true}/>} />
+                  <Route path="/" element={<Home />}/>
+              </Routes>
+          </div>
+          </Router>
+      </>
+  )
+}
+
+function MenuButton() {
+    return (
+        <>
+            <button id="menu-button">Menu</button>
+        </>
+    )
+}
---
+++ src/ItemPage.jsx
@@ -0,0 +1,9 @@
+export default function ItemPage({itemType}) {
+    return (
+    <>
+        <div>
+            <h2>Item Page</h2>
+        </div>
+    </>
+        )
+}
