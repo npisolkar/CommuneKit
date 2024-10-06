9d2709dc05aea26dac3053de5182c17c715ae313
RohanGaddam <129432188+RohanGaddam@users.noreply.github.com>
2024-10-06 15:53:27 -0400

    created add, get, update, and delete apis for user

--- CommuneKit-Backend/src/main/java/com/example/CommuneKitBackendTest/service/impl/UserServiceImpl.java
+++ CommuneKit-Backend/src/main/java/com/example/CommuneKitBackendTest/service/impl/UserServiceImpl.java
@@ -2,11 +2,15 @@ package com.example.CommuneKitBackendTest.service.impl;
 
 import com.example.CommuneKitBackendTest.dto.UserDto;
 import com.example.CommuneKitBackendTest.entity.User;
+import com.example.CommuneKitBackendTest.exception.ResourceNotFoundException;
 import com.example.CommuneKitBackendTest.mapper.UserMapper;
 import com.example.CommuneKitBackendTest.repository.UserRepository;
 import com.example.CommuneKitBackendTest.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -17,5 +21,40 @@ public class UserServiceImpl implements UserService {
         User user = UserMapper.mapToUser(userDto);
         User savedUser = userRepository.save(user);
         return UserMapper.mapToUserDto(savedUser);
+    }
+
+    @Override
+    public UserDto getUserById(long userID) {
+        User user = userRepository.findById(userID).orElseThrow(() -> new ResourceNotFoundException("User with given ID does not exist: " + userID));
+        return UserMapper.mapToUserDto(user);
+    }
+
+    @Override
+    public List<UserDto> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return users.stream().map((user) -> UserMapper.mapToUserDto(user)).collect(Collectors.toList());
+    }
+
+    @Override
+    public UserDto updateUser(Long userID, UserDto updatedUser) {
+        User user = userRepository.findById(userID).orElseThrow(() -> new ResourceNotFoundException("User with given ID not found: " + userID));
+
+        user.setUserName(updatedUser.getUserName());
+        user.setPassword(updatedUser.getPassword());
+        user.setEmail(updatedUser.getEmail());
+        user.setPhone(updatedUser.getPhone());
+        user.setAddress(updatedUser.getAddress());
+        user.setBio(updatedUser.getBio());
+
+        User updatedUserObj = userRepository.save(user);
+
+        return UserMapper.mapToUserDto(updatedUserObj);
+    }
+
+    @Override
+    public void deleteUser(Long userID) {
+        User user = userRepository.findById(userID).orElseThrow(() -> new ResourceNotFoundException("User with given ID not found: " + userID));
+
+        userRepository.deleteById(userID);
     }
 }
