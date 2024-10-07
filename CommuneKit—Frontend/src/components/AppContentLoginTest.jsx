42e6d6a577ba168bae9597f8b8cfb47843e99ca6
npisolkar <90062644+npisolkar@users.noreply.github.com>
2024-10-06 21:49:18 -0400

    AppContent, portion put behind authenticator

---
+++ CommuneKit—Frontend/src/components/AppContentLoginTest.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import AuthContent from "./authContent.jsx";
+
+function AppContentLoginTest() {
+    return(
+            <div>
+                <h2> Welcome content </h2>
+                <AuthContent/>
+            </div>
+    );
+};
+
+export default AppContentLoginTest;
