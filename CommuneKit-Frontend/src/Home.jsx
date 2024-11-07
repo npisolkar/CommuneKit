cd943f7a5fc254bcffd114c36542d46a00d07219
Edward Bi <bi43@purdue.edu>
2024-11-07 13:00:26 -0500

    create items button on home

--- CommuneKit-Frontend/src/Home.jsx
+++ CommuneKit-Frontend/src/Home.jsx
@@ -2,7 +2,7 @@
 import './styles.css'
 import ItemTable from './ItemTable.jsx'
 import {useEffect, useState} from 'react'
-import { useNavigate } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import {getAllItems, getItemsByUser, getMyBorrows} from "./services/ItemService.jsx";
 
 export default function Home() {
@@ -55,13 +55,18 @@ export default function Home() {
     return (
         <>
             <div className="home-items" id="posted-header">
-                <ItemTable headName="My Posted Items" items={postedItems} userID={localStorage.getItem("userID")} />
+                <ItemTable headName="My Posted Items" items={postedItems} userID={localStorage.getItem("userID")}/>
             </div>
             {/*<div className="home-items" id="borrowed-header">*/}
             {/*    <ItemTable headName="My Borrowed Items" items={borrowedItems} userID={localStorage.getItem("userID")} />*/}
             {/*</div>*/}
             <div className="home-items" id="suggested-items">
-                <ItemTable headName="Suggested Items" items={suggestedItems} userID={localStorage.getItem("userID")} />
+                <ItemTable headName="Suggested Items" items={suggestedItems} userID={localStorage.getItem("userID")}/>
+            </div>
+            <div id="create-item-home">
+                <Link to="/newitem">
+                    <button>Create New Item</button>
+                </Link>
             </div>
         </>
     );
