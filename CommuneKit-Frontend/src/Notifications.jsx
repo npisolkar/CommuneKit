5ccdf723a282b7a00df63cf4771983743b609e74
Ashe-w <ashley.w.cwc@gmail.com>
2024-11-21 12:16:28 -0500

    owner page now working completely

--- CommuneKit-Frontend/src/Notifications.jsx
+++ CommuneKit-Frontend/src/Notifications.jsx
@@ -69,6 +69,7 @@ export default function Notifications() {
         } catch (error) {
             console.error('Error:', error);
         }
+        window.location.reload();
     };
 
     const formatRequestBorrower = (request) => {
